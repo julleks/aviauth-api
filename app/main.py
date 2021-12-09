134a312c0331d8b2934d0c343a834efe497ee9d2
Alex Rozum <rozumalex@gmail.com>
2021-12-09 22:32:58 +0100

    feat: includ api_router to app

--- app/main.py
+++ app/main.py
@@ -1,8 +1,11 @@
 from fastapi import FastAPI
 from app.core.config import settings
+from app.api import api_router
 
 
 app = FastAPI(
     title=settings.PROJECT_NAME,
     version=settings.LATEST_VERSION,
 )
+
+app.include_router(api_router)
