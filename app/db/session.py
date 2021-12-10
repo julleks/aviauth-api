4ead2409ce9316d3c908e1c7d633e179530104ce
Alex Rozum <rozumalex@gmail.com>
2021-12-10 22:37:07 +0100

    style: remove unused imports

--- app/db/session.py
+++ app/db/session.py
@@ -1,6 +1,5 @@
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker
-from sqlmodel import SQLModel
 
 from app.core.config import settings
 
--- app/main.py
+++ app/main.py
@@ -1,10 +1,7 @@
-from fastapi import Depends, FastAPI
-from sqlalchemy import select
-from sqlalchemy.ext.asyncio import AsyncSession
+from fastapi import FastAPI
 
 from app.api import api_router
 from app.core.config import settings
-from app.db.session import get_session
 
 app = FastAPI(
     title=settings.PROJECT_NAME,
